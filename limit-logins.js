8c92e7aad82dc22062b860bffb8d578f41f6a050
jr00ck <jeremy@webguysaz.com>
2014-09-18 13:42:11 -0700

    renaming plugin & re-prefixing code
    Removed “WG” from plugin, files, and re-prefixed all code from “wg” to
    “ll”, as this is a general purpose plugin, not project specific (needs
    to be tested still)

--- limit-logins.js
+++ limit-logins.js
@@ -2,7 +2,7 @@
 
     // Hook into the heartbeat-send
     $(document).on('heartbeat-send', function(e, data) {
-        data['wg_heartbeat'] = 'stay_logged_in';
+        data['ll_heartbeat'] = 'stay_logged_in';
     });
 
     // Listen for the custom event "heartbeat-tick" on $(document).
@@ -11,9 +11,9 @@
         // Check if response says user is still logged in
         if ( data['wp-auth-check'] === false ) {
 
-            if(WGLL.logged_in == true){
+            if(LL.logged_in == true){
             	
-            	WGLL.logged_in = false;
+            	LL.logged_in = false;
             	showDialog();
         	}
         }
@@ -21,16 +21,16 @@
 
     function showDialog(){
     	var count = 30; // logout timer count
-		$('body').append('<div id="wg-dialog-message"><i class="icon-exclamation-sign icon-4x pull-left"></i>\
+		$('body').append('<div id="ll-dialog-message"><i class="icon-exclamation-sign icon-4x pull-left"></i>\
 				<p>You have been logged out. This is likely because your username has logged in from another device.</p>\
-				<p id="wg-timer-redirect">Redirecting to login page in <span id="wg-timer">' + count + '</span></p>\
+				<p id="ll-timer-redirect">Redirecting to login page in <span id="ll-timer">' + count + '</span></p>\
 			</div>');
-    	$('#wg-dialog-message').dialog({
+    	$('#ll-dialog-message').dialog({
     		modal: true,
     		buttons: {
     			Login: function(){
     				// redirect to login page w/ redirect back to current page
-    				location.href=WGLL.login_url;
+    				location.href=LL.login_url;
     			}
     		},
     		draggable: false,
@@ -50,9 +50,9 @@
 			if (count <= 0) {
 				clearInterval(counter);
 				//counter ended, redirect
-				location.href=WGLL.login_url;
+				location.href=LL.login_url;
 			}
-  			$('#wg-timer').html(count);
+  			$('#ll-timer').html(count);
 		}, 1000); //1000 will run it every 1 second
     }
 }(jQuery));
--- limit-logins.php
+++ limit-logins.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: WG Limit Logins
+Plugin Name: Limit Logins
 Description: Prevent multiple users from sharing the same account. Using Heartbeat API to kick out old logins.
-Version: 2.1
+Version: 2.2
 Author: Web Guys
 Author URI: http://webguysaz.com
 Contributors: jr00ck
@@ -22,22 +22,22 @@ Contributors: jr00ck
 
 const VERSION = '2.1';
 
-function wg_set_user_logged_in_status( $logged_in_cookie, $expire, $expiration, $user_id, $status = 'logged_in' ) {
+functionllset_user_logged_in_status( $logged_in_cookie, $expire, $expiration, $user_id, $status = 'logged_in' ) {
 
 	if ( ! empty( $user_id ) ) :
 
-		$data = get_transient( 'wg_user_logged_in_' . $user_id );
+		$data = get_transient( 'll_user_logged_in_' . $user_id );
 
 		if( false === $data )
 			$data = array();
 
 		$data[] = $logged_in_cookie;
 
-		set_transient( 'wg_user_logged_in_' . $user_id, $data );
+		set_transient( 'll_user_logged_in_' . $user_id, $data );
 
 	endif;
 }
-add_action( 'set_logged_in_cookie', 'wg_set_user_logged_in_status', 10, 5 );
+add_action( 'set_logged_in_cookie', 'll_set_user_logged_in_status', 10, 5 );
 
 
 
@@ -56,12 +56,12 @@ add_action( 'set_logged_in_cookie', 'wg_set_user_logged_in_status', 10, 5 );
  * @return      void
 */
 
-function wg_can_user_be_logged_in() {
+function ll_can_user_be_logged_in() {
 	if ( is_user_logged_in() ) :
 
 		$user_id = get_current_user_id();
 
-		$already_logged_in = get_transient( 'wg_user_logged_in_' . $user_id );
+		$already_logged_in = get_transient( 'll_user_logged_in_' . $user_id );
 
 		if( $already_logged_in !== false ) :
 
@@ -75,7 +75,7 @@ function wg_can_user_be_logged_in() {
 
 			if( ! in_array( $_COOKIE[LOGGED_IN_COOKIE], $data ) ) :
 
-				set_transient( 'wg_user_logged_in_' . $user_id, $data );
+				set_transient( 'll_user_logged_in_' . $user_id, $data );
 
 				// Log the user out - this is the oldest user logged into this account
 				wp_logout();
@@ -87,20 +87,20 @@ function wg_can_user_be_logged_in() {
 		
 	endif;
 }
-add_action( 'init', 'wg_can_user_be_logged_in' );
+add_action( 'init', 'll_can_user_be_logged_in' );
 
 /***********************************************************************
 * NEW CODE THAT USES HEARTBEAT API TO LOGOUT AN ALREADY LOGGED IN USER *
 ************************************************************************/
 
 // Load the heartbeat JS
-function wg_heartbeat_enqueue( $hook_suffix ) {
+function ll_heartbeat_enqueue( $hook_suffix ) {
     // Make sure the JS part of the Heartbeat API is loaded.
     wp_enqueue_script( 'heartbeat' );
-    wp_enqueue_script( 'wg-limit-logins', plugins_url( 'wg-limit-logins.js', __FILE__ ), array('jquery-ui-dialog'), VERSION );
-    wp_enqueue_style( 'wg-limit-logins', plugins_url( 'wg-limit-logins.css', __FILE__ ), false, VERSION );
+    wp_enqueue_script( 'll-limit-logins', plugins_url( 'll-limit-logins.js', __FILE__ ), array('jquery-ui-dialog'), VERSION );
+    wp_enqueue_style( 'll-limit-logins', plugins_url( 'll-limit-logins.css', __FILE__ ), false, VERSION );
     // output localized variables
-    wp_localize_script( 'wg-limit-logins', 'WGLL', array(
+    wp_localize_script( 'll-limit-logins', 'LL', array(
 		'logged_in'		=> is_user_logged_in(),
 		'login_url'		=> wp_login_url("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]")
 		)
@@ -109,19 +109,19 @@ function wg_heartbeat_enqueue( $hook_suffix ) {
     $jQueryUIver = '1.10.1';
     wp_enqueue_style('jquery-style', '//ajax.googleapis.com/ajax/libs/jqueryui/'.$jQueryUIver.'/themes/smoothness/jquery-ui.min.css', false, $jQueryUIver);
 }
-add_action( 'wp_enqueue_scripts', 'wg_heartbeat_enqueue' );
+add_action( 'wp_enqueue_scripts', 'll_heartbeat_enqueue' );
 
 
 // Modify the data that goes back with the heartbeat-tick
-function wg_heartbeat_received( $response, $data ) {
+function ll_heartbeat_received( $response, $data ) {
  
  		// Call function to check if user is still logged in
-        wg_can_user_be_logged_in();
+        ll_can_user_be_logged_in();
         
         // Send back if user is still logged in or not (not needed)
-        // $response['wg-logged-in'] = $logged_in ?: false;
+        // $response['ll-logged-in'] = $logged_in ?: false;
  
     // }
     return $response;
 }
-add_filter( 'heartbeat_received', 'wg_heartbeat_received', 10, 2 );
+add_filter( 'heartbeat_received', 'll_heartbeat_received', 10, 2 );
